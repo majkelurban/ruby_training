a73162db56f1a747db013e0beb16a1ea6de1b17d
Michal Urban <majellurban@gmail.com>
2019-11-12 10:53:31 +0100

    Init commit

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,60 @@
+class PortfoliosController < ApplicationController
+    before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+    layout 'portfolio'
+
+    def index
+        @portfolio_items = Portfolio.all
+    end
+
+    def show
+    end
+
+    def new
+        @portfolio_item = Portfolio.new
+        3.times {@portfolio_item.technologies.build}
+    end
+
+    def create
+        @portfolio_item = Portfolio.new(portfolio_params)
+
+        respond_to do |format|
+            if @portfolio_item.save
+                format.html { redirect_to portfolios_path, notice: 'Your portfolio has been created'}
+            else
+                format.html {render :new}
+            end
+        end 
+    end
+
+    def edit
+    end
+
+    def update
+        respond_to do |format|
+            if @portfolio_item.update(portfolio_params)
+                format.html { redirect_to portfolios_path, notice: 'Your portfolio succesfully updated'}
+            else
+                format.html {render :edit}
+            end
+        end 
+    end
+
+    def destroy
+        @portfolio_item.destroy
+
+        respond_to do |format|
+            format.html { redirect_to portfolios_path, notice: "Portfolio item deleted"}
+        end
+    end
+
+    private def portfolio_params
+        params.require(:portfolio).permit(  :title, 
+                                            :subtitle, 
+                                            :body, 
+                                            technologies_attributes: [:name])
+    end
+
+    def set_portfolio_item
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+end
