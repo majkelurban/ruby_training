d69f229ec1165fa815788a62398e8042099bf5b3
Michal Urban <majellurban@gmail.com>
2019-11-15 14:02:30 +0100

    Drag nad drop feature

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,10 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-
-//= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery_ujs
+//= require jquery-ui
+//= require html.sortable
+//= require turbolinks
+//= require_tree .
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,10 +1,18 @@
 class PortfoliosController < ApplicationController
     before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
-    access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+    access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
     layout 'portfolio'
 
     def index
         @portfolio_items = Portfolio.by_position
+    end
+
+    def sort
+        params[:order].each do |key, value|
+            Portfolio.find(value[:id]).update(position: value[:position])
+        end
+
+        render nothing: true
     end
 
     def show
